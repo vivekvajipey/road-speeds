97ddc6750853db035dc07733097b66106c3e008d
vivekvajipey <vivek.vajipey@gmail.com>
2023-12-11 14:49:01 -0800

    Created main.sh script to run experiments

--- main.sh
+++ main.sh
@@ -3,16 +3,22 @@
 # Activate the Python environment
 source road_env/bin/activate
 
+# No Sequence Model: Fully Connected Layer
+python nn_train.py --model resnet50 --pretrain imagenet --hidden_dim 128 --batch_size 32 --lr 0.003 --dropout_rate 0.1 --weight_decay 0.0001 --n_epochs 50
+
 # Sequence Model: No Pretraining
 echo "Sequence Model: No Pretraining"
-python seq_nn_train.py --model resnet18 --pretrain None --hidden_dim 128 --batch_size 32 --lr 0.003 --dropout_rate 0.1 --weight_decay 0.0001 --n_epochs 50
+python seq_nn_train.py --model resnet50 --pretrain None --hidden_dim 128 --batch_size 32 --lr 0.003 --dropout_rate 0.1 --weight_decay 0.0001 --n_epochs 50
 
 # Sequence Model: Pretraining with ImageNet
 echo "Sequence Model: Pretraining with ImageNet"
-python seq_nn_train.py --model resnet18 --pretrain imagenet --hidden_dim 128 --batch_size 32 --lr 0.003 --dropout_rate 0.1 --weight_decay 0.0001 --n_epochs 50
+python seq_nn_train.py --model resnet50 --pretrain imagenet --hidden_dim 128 --batch_size 32 --lr 0.003 --dropout_rate 0.1 --weight_decay 0.0001 --n_epochs 50
 
-# Sequence Model: Pretraining with Sentinel-2 self-supervised
+# Best Sequence Model: Pretraining with Sentinel-2 self-supervised, 
 echo "Sequence Model: Pretraining with Sentinel-2 self-supervised"
-python seq_nn_train.py --model resnet18 --pretrain sentinel2 --hidden_dim 128 --batch_size 32 --lr 0.003 --dropout_rate 0.1 --weight_decay 0.0001 --n_epochs 50
+python seq_nn_train.py --model resnet50 --pretrain sentinel2 --hidden_dim 128 --batch_size 32 --lr 0.003 --dropout_rate 0.1 --weight_decay 0.0001 --n_epochs 50
+
+# Sequence Model: Pretraining with Sentinel-2 self-supervised using GRU
+python seq_nn_train.py --model resnet50 --pretrain imagenet --hidden_dim 128 --rnn_type GRU --batch_size 32 --lr 0.003 --dropout_rate 0.1 --weight_decay 0.0001 --n_epochs 50
 
 echo "All experiments completed."
